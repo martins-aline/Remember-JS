8e35b43ddfae5245ae439f413dc294b383f6afa3
martins-aline <alines.martins97@gmail.com>
2024-10-21 21:34:36 -0300

    adiciona intro JS

---
+++ Introdução JS/code.js
@@ -0,0 +1,45 @@
+ alert("Arquivo externo");
+document.write("Hello Word");
+console.log("whats it's?");
+prompt("Qual seu nome?", "Digite aqui");
+confirm("Deseja cancelar? "); 
+
+//Declaração de variáveis
+
+var nome = "Aline";
+var idade = "27";
+var altura = "1.70";
+
+console.log("Nome " + nome);
+console.log("Idade " + idade);
+console.log("Altura " + altura); 
+
+// Operadores de comparação
+
+var x = 5;
+var y = "5";
+
+if (x === y) {
+    alert("X é igual.");
+} else {
+    alert("Não são iguais");
+}
+
+//Operadores Lógicos
+
+// Dado que x = 6 e y = 3:
+
+var x = 6;
+var y = 3;
+
+  if ( x < 10 && y > 1) {
+    alert("True")
+} else {
+    alert("False");
+} 
+
+     if ( x == 5 || y == 5) {
+        alert("True")
+    } alert ("False"); 
+
+
